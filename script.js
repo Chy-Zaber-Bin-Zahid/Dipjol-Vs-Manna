5478175c0c776744d2f2e35da34e42c0700703ef
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-05-15 19:25:16 +0600

    added mp3 sound

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function playSound(volume) {
     currentAudio.pause();
     currentAudio.currentTime = 0; // Reset to beginning
   }
-  const audio = new Audio("/cow.wav");
+  const audio = new Audio("/cowm.mp3");
   audio.volume = volume;
   audio.play();
   currentAudio = audio;
